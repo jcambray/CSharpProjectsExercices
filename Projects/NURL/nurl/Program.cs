504d51cbe7cf5c16e778ac17a26fca685e2890d3
jcambray <jerome.cambray@gmail.com>
2014-05-27 13:53:02 +0200

    Projet NURL

---
+++ Projects/NURL/nurl/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace nurl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new NurlApplication();
+            Console.Read();
+            //app.ParseArgument(args);
+        }
+
+
+
+    }
+
+    public class NurlApplication
+    {
+        String url = @"http://api.openweathermap.org/data/2.5/weather?q=paris&units=metric";
+
+        public NurlApplication()
+        {
+            //SaveAsJSON(GetFileName(url), GetData(url));
+            testConnectionTime(url,"5");
+        }
+
+        public void ParseArgument(string[] strgs)
+        {
+            for (int i = 0; i < strgs.Length; i++)
+            {
+                Console.WriteLine(strgs.ElementAt<string>(i));
+            }
+        }
+
+        private string GetData(string url)
+        {
+            WebClient client = new WebClient();
+            var datas = client.DownloadString(url);
+            return datas;
+        }
+
+        public void SaveAsJSON(string path, string datas)
+        {
+            try
+            {
+                File.WriteAllText(path + ".json", datas);
+                Console.WriteLine("save successful!!!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public string GetFileName(string url)
+        {
+            string[] urlParts = url.Split('/');
+            return urlParts[2];
+        }
+
+        public void testConnectionTime(string url, string times)
+        {
+            int t;
+            int.TryParse(times, out t);
+
+            if (t == 0)
+            {
+                Console.WriteLine("Invalid argument");
+                return;
+            }
+
+            WebClient client = new WebClient();
+            
+            for (int i = 0; i < t; i++)
+            {
+                var timeAtStart = DateTime.Now;
+                GetData(url);
+                var timeAtEnd = DateTime.Now;
+                var timespan = timeAtEnd - timeAtStart;
+                Console.WriteLine("Temps écoulé : " + timespan.TotalMilliseconds.ToString() + " millisecondes.");
+            }
+        }
+
+        public void AvgTestConnection(string url, string time)
+        {
+
+        }
+
+
+    }
+
+}
---
+++ Projects/NURL/nurlTests/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using nurl;
+using System.Net;
+using NUnit.Framework;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace nurlTests
+{
+    [TestFixture]
+    class NurlTest
+    {
+        [Test]
+        public void StringVariableShouldNotBeEmpty()
+        {
+            WebClient client = new WebClient();
+            var url = @"http://api.openweathermap.org/data/2.5/weather?q=paris&units=metric";
+            var data = client.DownloadString(url);
+            Assert.IsTrue(data.Length != 0);
+        }
+
+        [Test]
+        public void FileShouldNotBeEmpty()
+        {
+            NurlApplication app = new NurlApplication();
+            string data = "tototatatiti";
+            string file = "testFile";
+            string expectedfile = file+".json";
+            app.SaveAsJSON(file, data);
+            Assert.IsTrue(new FileInfo(expectedfile).Length > 0);
+        }
+    }
+}
